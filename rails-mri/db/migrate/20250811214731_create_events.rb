6fbdca2c14b55912c0ebc18676b7d6886efd3e1a
Alexandro castro <alexandrocastro@mac.home>
2025-08-12 15:47:27 +0100

    remove precision

--- rails-mri/db/migrate/20250811214731_create_events.rb
+++ rails-mri/db/migrate/20250811214731_create_events.rb
@@ -4,7 +4,7 @@ class CreateEvents < ActiveRecord::Migration[8.0]
       t.integer  :mall_id,         null: false
       t.integer  :kiosk_id,        null: false
       t.string   :event_type,      null: false
-      t.datetime :event_ts,        null: false, precision: 6
+      t.datetime :event_ts,        null: false
       t.integer  :amount_cents
       t.integer  :total_items
       t.string   :payment_method
